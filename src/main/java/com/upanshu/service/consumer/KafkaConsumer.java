8bc3a925edc4eef827fcd44eb20c6d9097b9a3ed
Narasimha Vuppala <nraovuppala@gmail.com>
2024-03-22 00:46:27 +0530

    Spring Boot upgraded

--- src/main/java/com/upanshu/service/consumer/KafkaConsumer.java
+++ src/main/java/com/upanshu/service/consumer/KafkaConsumer.java
@@ -4,9 +4,12 @@ import com.upanshu.service.dao.ProtagonistDao;
 import com.upanshu.service.event.Protagonist;
 import com.upanshu.service.event.database.ProtagonistInformation;
 import com.upanshu.service.processor.EventMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -23,9 +26,9 @@ public class KafkaConsumer {
         this.eventMapper = eventMapper;
     }
 
-    @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "group_id")
-    public void consume(@Payload Protagonist protagonist) {
-        logger.info("Consumed Message from show -> {}", protagonist.getShowName());
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}",concurrency = "3",groupId = "group_id" )
+    public void consume(@Payload ConsumerRecord<String,Protagonist> payload, Acknowledgment ack) {
+        logger.info("Consumed Message from show -> {}", payload.value().getShowName());
         ProtagonistInformation protagonistInformation = eventMapper.mapProtagonistToProtagonistInformation(protagonist);
         protagonistDao.insertProtagonistData(protagonistInformation);
         logger.info("Message received from consumer 1");
